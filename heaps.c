dade692f4dfa0f92ad9688681caa49b9f987d7b1
Pincho <brunopincho@gmail.com>
2016-10-05 15:24:22 +0100

    heap sort e arrays funcionais

--- heaps.c
+++ heaps.c
@@ -1,17 +1,17 @@
 /* Double-Click To Select Code */
  
-#include<stdio.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
  
-void heapsort(int[], int);
-void heapify(int[], int);
-void adjust(int[], int);
+void heapsort(float array[], int n);
+void heapify(float array[], int n);
+void adjust(float array[], int n);
  
-
- 
-void heapsort(int array[], int n)
+void heapsort(float array[], int n)
 {
- int i,t;
+ int i;
+ float t;
  
  heapify(array,n);
  
@@ -25,29 +25,31 @@ void heapsort(int array[], int n)
 }
  
  
-void heapify(int array[], int n)
+void heapify(float array[], int n)
 {
- int item,i,j,k;
+ float item;
+ int i,j,k;
  
- for(k=1 ; k<n ; k++)
- {
-  item = array[k];
-  i = k;
-  j = (i-1)/2;
- 
-  while( (i>0) && (item>array[j]) )
-  {
-   array[i] = array[j];
-   i = j;
-   j = (i-1)/2;
-  }
-  array[i] = item;
- }
+     for(k=1 ; k<n ; k++)
+     {
+      item = array[k];
+      i = k;
+      j = (i-1)/2;
+     
+      while( (i>0) && (item>array[j]) )
+      {
+       array[i] = array[j];
+       i = j;
+       j = (i-1)/2;
+      }
+      array[i] = item;
+     }
 }
  
-void adjust(int array[], int n)
+void adjust(float array[], int n)
 {
-       int item,i,j;
+       float item;
+       int i,j;
        
        j = 0;
        item = array[j];
@@ -70,10 +72,46 @@ void adjust(int array[], int n)
        array[j] = item;
 }
 
+
+void huffmanCode(char* symbols,float* freq,int** Code){
+  float matrixaux[strlen(symbols)][2];
+  float arrayAux[strlen(symbols)];
+  int i;
+
+  for(i=0;i<strlen(symbols);i++){
+      matrixaux[i][0] = symbols[i];
+      matrixaux[i][1] = freq[i];
+      arrayAux[i]=freq[i];
+  }
+
+  heapsort(arrayAux,6);
+  for(i = 0; i < strlen(symbols); i++)
+ {
+  printf("\n %4f", arrayAux[i]);
+ }
+
+
+
+}
+
+
+
+
 int main()
 {
- int array[50],n,i;
- 
+ int *array;
+ int n,i;
+ char  symbols[6]={'a','b','c','d','e','f'};
+ float freq[6] = {0.22,0.03,0.14,0.14,0.41,0.06};
+int * Code;
+int tamanh = sizeof(freq)/sizeof(float);
+Code = (int*)malloc(tamanh*sizeof(int));
+
+printf("tamanho do code %d",tamanh);
+
+huffmanCode(symbols,freq,&Code);
+/*
+ array = (int*)malloc(200*sizeof(int)); 
  
  printf("Enter the no. of elements to be sorted: ");
  scanf("%d",&n);
@@ -97,6 +135,8 @@ int main()
   printf("%4d", array[i]);
  }
  printf("\n");
+ printf("size of array %lu",sizeof(array));
+ free(array);*/
  
  return 0;
 }
